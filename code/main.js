a68c21d6635a831f1806d604ada16ea9f2f718e5
GustavoAndretto <gustavoandretto@outlook.com>
2021-09-17 12:01:41 -0300

    Add files via upload
    update

--- code/main.js
+++ code/main.js
@@ -1,161 +1,226 @@
-var elements = {
-    name: document.getElementById("name"),
-    netUser: document.getElementById("netUser"),
-    employeeId: document.getElementById("employeeId"),
-    phone: document.getElementById("phone"),
-    help: document.getElementById("help"),
-    troubleshooting: document.getElementById("troubleshooting"),
-    note: document.getElementById("note"),
-    avaliable: document.getElementById("avaliable"),
-    locale: document.getElementById("locale"),
-    address: document.getElementById("address"),
-    callStart: document.getElementById("callStart"),
-    callEnd: document.getElementById("callEnd"),
-    callNum: document.getElementById("callNum"),
-    template: document.getElementById("template"),
-    btnGenerate: document.getElementById("btnGenerate"),
-    btnCopy: document.getElementById("btnCopy"),
-    btnSave: document.getElementById("btnSave"),
-    btnClear: document.getElementById("btnClear")
+var dbTemplate = localforage.createInstance({ name: 'Template' });
+var dbConfig = localforage.createInstance({ name: 'Config' });
+
+function formatLocation(street, number, misc, neighborhood, city, state, cep) {
+    return `${street}, ${number} ${misc}, ${neighborhood}, ${city} - ${state} - CEP ${cep}`;
 }
 
-function formatTime(date) 
-{
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
+new Vue({ 
+    el: '#app', 
+    vuetify: new Vuetify({
+        theme: { dark: false }
+    }),
+    directives: { mask: VueMask.VueMaskDirective },
+    data: {
+        about: {
+            author: "Gustavo Andretto",
+            page: "https://github.com/gustavoandretto",
+            version: "0.2",
+            revision: "Sep, 16, 2021"
+        },
+        mask: {
+            phone: '+55 (##) #####-####',
+            cep: '#####-###',
+            workTime: '##:## às ##:##',
+            time: '##:##'
+        },
+        form: {
+            employee: {
+                id: null,
+                name: null,
+                user: null,
+                phone: null,
+                time: null,
+                location: {
+                    cep: '',
+                    state: null,
+                    city: null,
+                    neighborhood: null,
+                    street: null,
+                    number: null,
+                    misc: null,
+                    desc: null,
+                    all: null
+                }
+            },
+            ticket: {
+                id: null,
+                description: null,
+                troubleshooting: null,
+                note: null,
+                time: { 
+                    start: null, 
+                    end: null
+                }
+            }
+        },
+        placeholder: { workLocation: ['Home Office', 'Cliente', 'Escritório'] },
+        error: { cep: [] },
+        view: { address: false, template: false },
+        validator: { address: false },
+        template: { input: null, items: [], text: null }
+    },
+    methods: {
+        resetForm() {
+            this.$refs.form.reset();
+        },
+        cepChanged(value) {
+            if(value.length == this.mask.cep.length) {
+                cep(value).then((result) => {
+                    this.form.employee.location.state = result['state'];
+                    this.form.employee.location.neighborhood = result['neighborhood'];
+                    this.form.employee.location.street = result['street'];
+                    this.form.employee.location.city = result['city'];
 
-    var strMin = minutes < 10 ? '0' + minutes : minutes;
-    var strHours = hours < 10 ? '0' + hours : hours;
+                    this.error.cep = [];
+                })
+                .catch((e) => {
+                    this.error.cep = ['CEP não encontrado'];
+                })
 
-    var strTime = strHours + ':' + strMin;
-    return strTime;
-}
+                return;
+            }
 
-function updateTemplate()
-{
-    var strTemplate = "";
+            this.error.cep = ['CEP inválido'];
+        },
+        updateForm(value) {
+            if(value) { 
+                this.form = value; 
+            }
+        },
+        initDefault() {
+            // TODO:
+        },
+        clearForm() {
+            this.$refs.form.reset();
 
-    if(elements.name.value){
-        strTemplate += "•Nome Completo: " + elements.name.value + "\n";
+            if(this.$refs.formAddress){
+                this.$refs.formAddress.reset();
+                this.$refs.formAddress.resetValidation();
+
+                // Reseta os erros de validação do cep.
+                this.error.cep = [];
+            }
+        },
+        insertAddress() {
+            this.$refs.formAddress.validate();
+
+            if(this.form.employee.location.number && this.form.employee.location.cep) {
+                var location = this.form.employee.location;
+
+                this.form.employee.location.all = formatLocation(location.street, location.number, !location.misc ? '' : location.misc,
+                location.neighborhood, location.city, location.state, location.cep);
+
+                this.view.address = false;
+            }
+        },
+        isNightTheme() {
+            return this.$vuetify.theme.dark == true;
+        },
+        setTheme(theme) {
+            // Troca o tema conforme o valor inserido, o valor deve ser do tipo boleano.
+            this.$vuetify.theme.dark = theme;
+            dbConfig.setItem('theme', theme);
+        },
+        switchTheme(theme) { 
+            this.setTheme(theme);
+            
+            // Remove o foco do botão após a troca do tema.
+            document.activeElement.blur();
+        },
+        loadTheme() {
+            // Promise que retorna o tema armazenado em cache.
+            dbConfig.getItem('theme').then((value) => {
+                this.setTheme(value);
+            });
+        },
+        updateTemplateList() {
+            // Promise que atualiza a lista de keys após a remoção do template.
+            dbTemplate.keys().then((keys) => {
+                this.template.items = keys;
+            });
+        },
+        makeTemplate() {
+            var strTemplate = '';
+
+            if(this.form.employee.name){
+                strTemplate += "•Nome Completo: " + this.form.employee.name + "\n";
+            }
+            if(this.form.employee.user) {
+                strTemplate += "•Usuário de Rede: " + this.form.employee.user + "\n";
+            }
+            if(this.form.employee.id) {
+                strTemplate += "•Matrícula: " + this.form.employee.id + "\n";
+            }
+            if(this.form.employee.phone) {
+                strTemplate += "•Telefone: " + this.form.employee.phone + "\n";
+            }
+            if(this.form.employee.location.all) {
+                strTemplate += "•Endereço: " + this.form.employee.location.all + "\n";
+            }
+            if(this.form.employee.time) {
+                strTemplate += "•Horário Disponível: " + this.form.employee.time + "\n";
+            }
+            if(this.form.employee.location.desc) {
+                strTemplate += "•Local de Trabalho: " + this.form.employee.location.desc + "\n\n";
+            }
+
+            console.log(this.form.ticket.description);
+
+            if(this.form.ticket.description) {
+                strTemplate += "•Descrição do Problema:\n" + this.form.ticket.description + "\n\n";
+            }
+            if(this.form.ticket.troubleshooting) {
+                strTemplate += "•Troubleshooting:\n" + this.form.ticket.troubleshooting + "\n\n";
+            }
+            if(this.form.ticket.note) {
+                strTemplate += "•Observações:\n" + this.form.ticket.note;
+            }
+
+            this.template.text = strTemplate;
+        },
+        saveTemplate() {
+            if(this.template.input) {
+                dbTemplate.setItem(this.template.input, this.form).then(() => {
+                    this.updateTemplateList();
+                });  
+            }
+        },
+        deleteTemplate() {
+            if(this.template.input) {
+                dbTemplate.removeItem(this.template.input).then(() => {
+                    // Atualiza a lista de templates.
+                    this.updateTemplateList();
+
+                    // Limpa o formulário após a exclusão do template.
+                    this.clearForm();
+                });
+            }
+        },
+        selectTemplate() {
+            if(this.template.input) {
+                dbTemplate.getItem(this.template.input).then((value) => {
+                    // Atualiza o formulário com o template selecionado.
+                    this.updateForm(value);
+                });
+            }
+        },
+        showPage() {
+            // Remove o elemento css 'hidden' da página
+            document.getElementById('app').classList.remove('hidden');
+        }
+    },
+    mounted() {
+        // Atualiza a lista de templates após a criação da instância do Vue.
+        this.updateTemplateList();
+
+        // Carrega o tema escolhido.
+        this.loadTheme();
+
+        // Inicializa os valores padrão do formulário
+        this.initDefault();
+
+        // Exibe a página
+        this.showPage();
     }
-    if(elements.netUser.value) {
-        strTemplate += "•Usuário de Rede: " + elements.netUser.value + "\n";
-    }
-    if(elements.employeeId.value) {
-        strTemplate += "•Matrícula:" + elements.employeeId.value + "\n";
-    }
-    if(elements.phone.value) {
-        strTemplate += "•Telefone: " + elements.phone.value + "\n";
-    }
-    if(elements.address.value) {
-        strTemplate += "•Endereço: " + elements.address.value + "\n";
-    }
-    if(elements.avaliable.value) {
-        strTemplate += "•Horário Disponível: " + elements.avaliable.value + "\n";
-    }
-    if(elements.locale.value) {
-        strTemplate += "•Local de Trabalho: " + elements.locale.options[elements.locale.value].text + "\n";
-    }
-    if(elements.help.value) {
-        strTemplate += "•Descrição detalhada:\n" + elements.help.value + "\n";
-    }
-    if(elements.troubleshooting.value) {
-        strTemplate += "•Troubleshooting:\n" + elements.troubleshooting.value + "\n";
-    }
-    if(elements.note.value) {
-        strTemplate += "•Observações:\n" + elements.note.value + "\n";
-    }
-
-    strTemplate = strTemplate.substring(0, strTemplate.length - 1);
-
-    elements.template.value = strTemplate;
-    elements.template.focus();
-    M.textareaAutoResize(elements.template);
-
-    M.toast({html: 'Template gerado<i class="material-icons dp48">check</i>'});
-}
-
-function copyToClipboard(){
-    var value = elements.template;
-
-    value.select();
-    value.setSelectionRange(0, 99999);
-
-    document.execCommand("copy");
-
-    M.toast({html: 'Copiado para área de transferência<i class="material-icons dp48">check</i>'});
-}
-
-function save() {
-    
-    if(elements.callNum.value) {
-        M.toast({html: 'Funcionalidade Indisponível<i class=\"material-icons dp48\">close</i>', classes: 'red lighten-1'});
-    }
-    else{
-        M.toast({html: 'Insira o número do chamado<i class=\"material-icons dp48\">close</i>', classes: 'red lighten-1'});
-    }
-}
-
-function clear() {
-    elements.name.value = null;
-    elements.netUser.value = null;
-    elements.employeeId.value = null;
-    elements.phone.value = null;
-    elements.help.value = null;
-    elements.troubleshooting.value = null;
-    elements.note.value = null;
-    elements.locale.selectedIndex = null;
-    elements.avaliable.value = null;
-    elements.address.value = null;
-    elements.callStart.value = formatTime(new Date());
-    elements.callEnd.value = null;
-    elements.callNum.value = null;
-    elements.template.value = null;
-
-    M.FormSelect.init(elements.locale);
-    M.updateTextFields();
-    M.textareaAutoResize(elements.template);
-    M.textareaAutoResize(elements.help);
-    M.textareaAutoResize(elements.note);
-    M.textareaAutoResize(elements.troubleshooting);
-    M.toast({html: 'Limpeza concluída<i class="material-icons dp48">check</i>'});    
-}
-
-function initialize()
-{
-    elements.phone.addEventListener("focus", function() {
-        phoneMask.mask(elements.phone);
-    });
-    elements.phone.addEventListener("blur", function() {
-        Inputmask.remove(elements.phone);
-    });
-    elements.avaliable.addEventListener("focus", function() {
-        avaliableMask.mask(elements.avaliable);
-    });
-    elements.avaliable.addEventListener("blur", function() {
-        Inputmask.remove(elements.avaliable);
-    });
-    elements.btnCopy.addEventListener("click", function() { 
-        copyToClipboard();
-    });
-    elements.btnSave.addEventListener("click", function() {
-        save();
-    });
-    elements.btnClear.addEventListener("click", function() {
-        clear();
-    });
-    elements.btnGenerate.addEventListener("click", function() {
-        updateTemplate();
-    });
-
-    var phoneMask = new Inputmask("+55 99 99999 9999", {placeholder: ''});
-    var avaliableMask = new Inputmask("99:99 às 99:99");
-
-    M.Timepicker.init(document.querySelectorAll('.timepicker'), {twelveHour:false});
-    M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
-    M.FormSelect.init(document.querySelectorAll('select'), {});
-
-    elements.callStart.value = formatTime(new Date());
-}
-
-initialize();
+})
